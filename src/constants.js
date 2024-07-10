939f7b4515cdf37d1d7ae47f1b403e2041dddbc4
Mas Kharisman <bimokharis1810@gmail.com>
2024-07-10 12:57:49 +0700

    first commit

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+const LOG_TYPE = {
+    GROUP: "GROUP",
+    VIDEO: "VIDEO",
+    VIDEO_URL: "VIDEO_URL",
+    PHOTO: "PHOTO",
+    PHOTO_URL: "PHOTO_URL",
+};
+
+const ERROR_TYPE = {
+    RATE_LIMIT: "Encountered Rate Limit 😢.",
+    FAILED: "Failed 😢.",
+};
+
+const ACTION = {
+    SEND_CHAT_ACTION: "sendChatAction",
+    SEND_MESSAGE: "sendMessage",
+    DELETE_MESSAGE: "deleteMessage",
+    SEND_VIDEO: "sendVideo",
+    SEND_PHOTO: "sendPhoto",
+    SEND_MEDIA_GROUP: "sendMediaGroup",
+};
+
+const SUCCESS_MESSAGE = {
+    GROUP: "Media group sent successfully ✅",
+    VIDEO: "Video sent successfully ✅",
+    VIDEO_URL: "Video url sent successfully ✅",
+    PHOTO: "Photo sent successfully ✅",
+    PHOTO_URL: "Photo url sent successfully ✅",
+};
+
+const MESSSAGE = {
+    HELLO: "Hello from InstaSaver Bot!",
+    WELCOME:
+        "Hi firstName, 👋\nWelcome to Insta Saver Bot! \n\nTo get started, send me the link of Instagram post, Reels, IGTV, etc. to download the video. \n\nHappy downloading!",
+    GATHERING_CONTENT: "Gathering content 🔍",
+    INITIATING_UPLOAD: "Initiating upload 🚀",
+    DOWNLOADING: "➡️  Downloading post for: shortCode 📥",
+    VIDEO_UPLOAD_LIMIT:
+        "Unable to send video 😢 \nPossibly, it might have exceeded the Bot's upload limit. \n\nPlease download the video from below link: \nmediaUrl",
+    PHOTO_UPLOAD_LIMIT:
+        "Unable to send photo 😢 \nPossibly, it might have exceeded the Bot's upload limit. \n\nPlease download the photo from below link: \nmediaUrl",
+};
+
+module.exports = {
+    LOG_TYPE,
+    ERROR_TYPE,
+    ACTION,
+    SUCCESS_MESSAGE,
+    MESSSAGE,
+};
